b5a022e8f2973ea7df239f4975588e3a35cc3a64
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2024-09-14 17:15:23 +0400

    remove fomr

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ContactForm from '../components/ContactForm/ContactForm';
+
 
 const page = () => {
   return (
     <>
-    <ContactForm />
+    
     </>
   )
 }
