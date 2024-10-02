5ec19806428e7c11b3a17fbdbf7c63c66286037a
adeel <adeelcomsats070@gmail.com>
2024-10-03 00:42:25 +0500

    package added emailjs

--- src/app/components/ContactUs/Hero.jsx
+++ src/app/components/ContactUs/Hero.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { UserIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { CiMail } from 'react-icons/ci';
@@ -6,8 +7,28 @@ import { FiPhoneCall } from "react-icons/fi";
 import { GiWorld } from "react-icons/gi";
 import { TiMessages } from "react-icons/ti";
 import { AiOutlineMessage } from "react-icons/ai";
+import emailjs from "emailjs-com";
 
 const ContactUsHeroSection = () => {
+    // const sendEmail = (e) => {
+    //     // e.preventDefault();
+
+    //     emailjs
+    //         .send(
+    //             "service_d17ip7e",    // Replace with your Service ID
+    //             "template_fe1ff54",   // Replace with your Template ID
+    //             { email: "adeelcomsats070@gmail.com" },
+    //             "hBEuQsrrqkn0STWmO"        // Replace with your User ID
+    //         )
+    //         .then(
+    //             (result) => {
+    //                 alert("Email sent successfully!");
+    //             },
+    //             (error) => {
+    //                 alert("Failed to send email. Try again later.");
+    //             }
+    //         );
+    // };
     return (
         <div>
             <section className="bg-gradient-to-r from-[#080a62] from-30%  to-secondary to-90% pt-16 md:px-4 text-white relative">
@@ -34,96 +55,97 @@ const ContactUsHeroSection = () => {
             {/* Form Section */}
             <div className=' relative -top-48'>
                 <div className="bg-white relative text-gray-700 rounded-3xl shadow-lg p-8 max-w-3xl mx-auto">
-                    <form>
-                        {/* First Name & Last Name */}
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-                            <div className='relative'>
-                                <UserIcon className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="text"
-                                    className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="First Name"
-                                />
-                            </div>
-                            <div className=' relative'>
-                                <UserIcon className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="text"
-                                    className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Last Name"
-                                />
-                            </div>
+                    {/* <form> */}
+                    {/* First Name & Last Name */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+                        <div className='relative'>
+                            <UserIcon className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                            <input
+                                type="text"
+                                className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                                placeholder="First Name"
+                            />
                         </div>
+                        <div className=' relative'>
+                            <UserIcon className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                            <input
+                                type="text"
+                                className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                                placeholder="Last Name"
+                            />
+                        </div>
+                    </div>
 
-                        {/* Email & Phone */}
-                        <div className="grid relative grid-cols-1 gap-6 mb-6">
-                            <CiMail className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                    {/* Email & Phone */}
+                    <div className="grid relative grid-cols-1 gap-6 mb-6">
+                        <CiMail className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                        <input
+                            type="email"
+                            className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                            placeholder="Email"
+                        />
+                    </div>
+
+                    <div className="grid grid-cols-3 gap-6 mb-6">
+                        <div className=' relative'>
+                            <BsGrid className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
                             <input
                                 type="email"
                                 className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                placeholder="Email"
+                                placeholder="Code"
                             />
                         </div>
-
-                        <div className="grid grid-cols-3 gap-6 mb-6">
-                            <div className=' relative'>
-                                <BsGrid className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="email"
-                                    className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Code"
-                                />
-                            </div>
-                            <div className='col-span-2 relative'>
-                                <FiPhoneCall className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="text"
-                                    className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Phone"
-                                />
-                            </div>
-                        </div>
-
-                        {/* Country & Query */}
-                        <div className="grid grid-cols-1 gap-6 mb-6">
-                            <div className='relative'>
-                                <GiWorld className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="text"
-                                    className="w-full px-4 pl-9 py-3 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Country"
-                                />
-                            </div>
-                            <div className=' relative'>
-                                <TiMessages className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                                <input
-                                    type="text"
-                                    className="w-full px-4 pl-9 py-3 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                    placeholder="Query"
-                                />
-                            </div>
-                        </div>
-
-                        {/* Message */}
-                        <div className="mb-6 relative">
-                            <AiOutlineMessage className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
-                            <textarea
+                        <div className='col-span-2 relative'>
+                            <FiPhoneCall className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                            <input
+                                type="text"
                                 className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
-                                rows="4"
-                                placeholder="Message"
-                            ></textarea>
+                                placeholder="Phone"
+                            />
                         </div>
+                    </div>
 
-                        {/* Submit Button */}
-                        <div className="text-center -mb-12">
-                            <button
-                                type="submit"
-                                className="bg-gradient-to-r from-[#080a62] to-[#dc3d52] text-white font-semibold py-3 px-8 rounded-full text-lg"
-                            >
-                                Submit
-                            </button>
+                    {/* Country & Query */}
+                    <div className="grid grid-cols-1 gap-6 mb-6">
+                        <div className='relative'>
+                            <GiWorld className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                            <input
+                                type="text"
+                                className="w-full px-4 pl-9 py-3 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                                placeholder="Country"
+                            />
                         </div>
-                    </form>
+                        <div className=' relative'>
+                            <TiMessages className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                            <input
+                                type="text"
+                                className="w-full px-4 pl-9 py-3 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                                placeholder="Query"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Message */}
+                    <div className="mb-6 relative">
+                        <AiOutlineMessage className='absolute top-4 left-3 text-gray-400 h-5 w-5' />
+                        <textarea
+                            className="w-full px-4 py-3 pl-9 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-primary"
+                            rows="4"
+                            placeholder="Message"
+                        ></textarea>
+                    </div>
+
+                    {/* Submit Button */}
+                    <div className="text-center -mb-12">
+                        <button
+                            // onClick={() => { sendEmail() }}
+                            type="submit"
+                            className="bg-gradient-to-r from-[#080a62] to-[#dc3d52] text-white font-semibold py-3 px-8 rounded-full text-lg"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                    {/* </form> */}
                 </div>
                 <div className='my-10'>
                     <img className='w-[400px] mx-auto ' src="/social.svg" alt="" />
